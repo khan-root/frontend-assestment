ae6faee5a34462ea093f8f18657edcec6a36b6c3
Sarmad Faizan <sarmadf49@gmail.com>
2025-05-31 20:12:34 +0500

    create form for add product

--- src/view/Product/AddProduct.jsx
+++ src/view/Product/AddProduct.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../../components/Input'
 
 const AddProduct = () => {
+
+    const [formValue, setFormValue] = useState({
+        name: '',
+        price: '',
+        image: '',
+        loading: false,
+    })
+
+
+    const handleChange = (e)=>{
+        const {name, value} = e.target
+        setFormValue((prevState)=>({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log(formValue)
+    }
+
   return (
-    <div>
-        <form>
-            <Input />
+    <div className='w-[40%] mx-auto'>
+        <form onSubmit={handleSubmit} className='border border-gray-500 rounded-md p-4 space-y-4'>
+            <div className='space-y-2'>
+                <label>Product Name</label>
+                <Input placeholder='Product Name' onChange={handleChange} />
+            </div>
+            <div className='space-y-2'>
+                <label>Product Price</label>
+                <Input placeholder='Product Price' onChange={handleChange} />
+            </div>
+            <div className='space-y-2'>
+                <label>Product Image</label>
+                <Input placeholder='Product Image Url' onChange={handleChange} />
+            </div>
+            <div className='flex items-center justify-center'>
+
+                <button className='bg-blue-500 text-white px-4 py-2 rounded-md'>Add Product</button>
+            </div>
         </form>
     </div>
   )
