a40ae5c7c776b394842ea78c828d3b6f62126b05
Sarmad Faizan <sarmadf49@gmail.com>
2025-05-31 20:59:20 +0500

    simple design cart page

--- src/view/Cart/Cart.jsx
+++ src/view/Cart/Cart.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { BiTrash } from 'react-icons/bi'
 
 const Cart = () => {
+    const data = {
+        image: "https://images.pexels.com/photos/20519105/pexels-photo-20519105/free-photo-of-checkered-paddles-and-balls.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name: "Product 1",
+        description: "Description 1",
+        price: 100,
+    }
   return (
-    <div>Cart</div>
+    <div className='w-[60%] mx-auto space-y-4'>
+        
+        <table className="min-w-full border border-gray-500 rounded-md">
+        <thead>
+            <tr className="bg-gray-100">
+            <th className="p-3 border-b text-left">Image</th>
+            <th className="p-3 border-b text-left">Name</th>
+            <th className="p-3 border-b text-left">Price</th>
+            <th className="p-3 border-b text-left">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            {[1, 2, 3].map((_, index) => (
+            <tr key={index} className="hover:bg-gray-50">
+                <td className="p-3 border-b">
+                <img
+                    src={data.image}
+                    alt={data.name}
+                    className="w-32 h-20 object-cover rounded"
+                />
+                </td>
+                <td className="p-3 border-b font-semibold">{data.name}</td>
+                <td className="p-3 border-b text-gray-600">{data.price}</td>
+                <td className="p-3 border-b">
+                    <span className='text-red-500 border border-red-500 rounded-md  w-[30px] h-[30px] text-[14px] flex items-center justify-center cursor-pointer'>
+                        <BiTrash />
+                    </span>
+                </td>
+            </tr>
+            ))}
+        </tbody>
+        </table>
+        <div className='flex items-center justify-between'>
+            <div className='flex items-center space-x-4'>
+                <span className='text-gray-500'>Total: <span className='font-bold'>$100</span></span>
+                <div className='flex items-center space-x-4'>
+                    <button className='bg-blue-500 text-white px-4 py-2 rounded-md'>Checkout</button>
+                    <button className='bg-red-500 text-white px-4 py-2 rounded-md'>Clear Cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
