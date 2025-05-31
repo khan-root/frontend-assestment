34ba7f515234528a4f79689e0d12cb4ef824c83c
Sarmad Faizan <sarmadf49@gmail.com>
2025-05-31 20:18:14 +0500

    main toaster file for errors, success messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routers } from './routes'
 import Navabr from './components/Navabr'
+import { Toaster } from './components/Toaster'
 
 const App = () => {
   return (
     <div className='h-screen w-screen'>
+      <Toaster />
       <Navabr />
       <Routers />
     </div>
---
+++ src/components/Toaster.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const Toaster = () => {
+  return (
+    <ToastContainer 
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+    />
+  )
+}
+
+
+const showToast = (message, type) => {
+    switch(type){
+        case 'success':
+            toast.success(message)
+            break
+        case 'error':
+            toast.error(message)
+            break
+        default:
+            toast.info(message)
+            break
+    }
+
+}
+
+
+export {Toaster, showToast}
